4eab97f412c1f61afed91d7212e01afee8697358
jagat <jagatrajsingh2000@gmail.com>
2023-02-13 10:09:20 +0530

    employee_data_code_using_callback function

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import {v4 as uuidv4} from 'uuid'
+import './input.css'
+import Self from './Selfapi'
+import { useState } from 'react';
+import Employee from './components/Employee';
 
 function App() {
+  const [role, setRole] = useState()
+  const [employees,setEmployees]=useState(Self)
+  const showEmployees = true
+
+  function updatedEmployees(id,newName,newRole){ 
+    const updatedList=employees.map((employee)=>{
+      if(id===employee.id){
+        console.log("employee with id match",employee)
+        return {...employee,name:newName,role:newRole}
+      }
+      console.log(employee)
+      return employee
+    })
+    setEmployees(updatedList)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='App'>
+        {showEmployees ? <><input type="text" onChange={(e) => { setRole(e.target.value)}}/>
+          <div className='flex flex-wrap justify-center'>
+          {employees.map((employee)=>{
+            return(
+            <Employee updatedEmployees={updatedEmployees} key={employee.id} id={employee.id} name={employee.name} role={employee.role} img={employee.img}/>
+          )}
+          )}
+          </div>
+        </> : <p>not able to see</p>}
+      </div>
+    </>
   );
 }
 
---
+++ src/Selfapi.js
@@ -0,0 +1,37 @@
+const Self=[{
+    id:1,
+    name:'calab',
+    role:'software developer',
+    img:'https://media.istockphoto.com/id/1364917563/photo/businessman-smiling-with-arms-crossed-on-white-background.jpg?s=1024x1024&w=is&k=20&c=O_h1ic7M0SWTC40NVzYUTLWE2Yy8511S8QPUGEUT9tE='
+  },
+  {
+    id:2,
+    name:'calab',
+    role:'software developer',
+    img:'https://media.istockphoto.com/id/1364917563/photo/businessman-smiling-with-arms-crossed-on-white-background.jpg?s=1024x1024&w=is&k=20&c=O_h1ic7M0SWTC40NVzYUTLWE2Yy8511S8QPUGEUT9tE='
+  },
+  {
+    id:3,
+    name:'calab',
+    role:'software developer',
+    img:'https://media.istockphoto.com/id/1364917563/photo/businessman-smiling-with-arms-crossed-on-white-background.jpg?s=1024x1024&w=is&k=20&c=O_h1ic7M0SWTC40NVzYUTLWE2Yy8511S8QPUGEUT9tE='
+  },
+  {
+    id:4,
+    name:'calab',
+    role:'software developer',
+    img:'https://media.istockphoto.com/id/1364917563/photo/businessman-smiling-with-arms-crossed-on-white-background.jpg?s=1024x1024&w=is&k=20&c=O_h1ic7M0SWTC40NVzYUTLWE2Yy8511S8QPUGEUT9tE='
+  },
+  {
+    id:5,
+    name:'calab',
+    role:'software developer',
+    img:'https://media.istockphoto.com/id/1364917563/photo/businessman-smiling-with-arms-crossed-on-white-background.jpg?s=1024x1024&w=is&k=20&c=O_h1ic7M0SWTC40NVzYUTLWE2Yy8511S8QPUGEUT9tE='
+  },
+  {
+    id:6,
+    name:'calab',
+    role:'software developer',
+    img:'https://media.istockphoto.com/id/1364917563/photo/businessman-smiling-with-arms-crossed-on-white-background.jpg?s=1024x1024&w=is&k=20&c=O_h1ic7M0SWTC40NVzYUTLWE2Yy8511S8QPUGEUT9tE='
+  }]
+  export default Self
